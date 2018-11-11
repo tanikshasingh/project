7c21f783f35ced838058cb060f66941d90feec62
tanikshasingh <43039758+tanikshasingh@users.noreply.github.com>
2018-11-11 05:13:42 -0600

    Update enquiry.sql

--- enquiry.sql
+++ enquiry.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS enquiry_database.enquiry (
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(20) null,
 email VARCHAR(20) null,
 mobile VARCHAR(10) null,
