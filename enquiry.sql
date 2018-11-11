616dc4128fc8634539d0fb7539ebc1d5a3855862
tanikshasingh <43039758+tanikshasingh@users.noreply.github.com>
2018-11-11 05:12:52 -0600

    Update enquiry.sql

--- enquiry.sql
+++ enquiry.sql
@@ -1,9 +1,7 @@
-CREATE TABLE IF NOT EXISTS enquiry_database.enquiry 
-(
+CREATE TABLE IF NOT EXISTS enquiry_database.enquiry (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(20) null,
 email VARCHAR(20) null,
 mobile VARCHAR(10) null,
 issue VARCHAR(50) null,
-)
-ENGINE = INNODB;
+)ENGINE=INNODB;
