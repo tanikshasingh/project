77b0b5955beded1d57d303d87a45d45501c4af81
tanikshasingh <43039758+tanikshasingh@users.noreply.github.com>
2018-11-11 05:20:07 -0600

    Update enquiry.sql

--- enquiry.sql
+++ enquiry.sql
@@ -3,5 +3,5 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(20) null,
 email VARCHAR(20) null,
 mobile VARCHAR(10) null,
-issue VARCHAR(50) null,
-)
+issues VARCHAR(50) null,
+)ENGINE=InnoDB;
