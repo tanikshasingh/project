231325e4a199ee4f18755751f2959d7cf5264271
tanikshasingh <43039758+tanikshasingh@users.noreply.github.com>
2018-11-11 05:46:12 -0600

    Delete enquiry.sql

--- enquiry.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE enquiry (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(20) null,
-email VARCHAR(20) null,
-mobile VARCHAR(10) null,
-issues VARCHAR(20) null,
-PRIMARY KEY (id),
-)ENGINE=InnoDB;
