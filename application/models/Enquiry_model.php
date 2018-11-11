8525531ace289bff4cb9edd5c1b69a0fff334b4b
tanikshasingh <43039758+tanikshasingh@users.noreply.github.com>
2018-11-11 03:19:53 -0600

    Update Enquiry_model.php

--- application/models/Enquiry_model.php
+++ application/models/Enquiry_model.php
@@ -3,7 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Enquiry_model extends CI_Model {
 
-	function save($data){
-		$this->db->insert('bookings',$data);
+	public function set_news()
+	{
+	    $this->load->helper('url');
+
+	    $data = array(
+	        'name' => $this->input->post('name'),
+	        'email' => $this->input->post('email'),
+	        'mobile' => $this->input->post('mobile'),
+	        'issues' => $this->input->post('issues')
+	    );
+
+	    return $this->db->insert('enquiry', $data);
 	}
 }
