64c188f91676da5694f70dca037d99899529ce23
tanikshasingh <tanikshasingh94@gmail.com>
2018-11-13 21:17:32 -0600

    added in enquiry

--- enquiry.sql
+++ enquiry.sql
@@ -3,5 +3,6 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(20) null,
 email VARCHAR(20) null,
 mobile VARCHAR(10) null,
-issues text null
+issues text null,
+created_at timestamp
 )ENGINE=innodb;
