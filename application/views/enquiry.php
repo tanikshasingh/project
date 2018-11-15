e7a7793c3fdd6c0d7b27a92fbc272c4cff4184d4
tanikshasingh <43039758+tanikshasingh@users.noreply.github.com>
2018-11-14 19:57:29 -0600

    Update enquiry.php

--- application/views/enquiry.php
+++ application/views/enquiry.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Submit Enquiry!</title>
+	<title>Submit Enquiry</title>
 
 	<style type="text/css">
 body
